975edaece5b2931bebbafa731b3919a26872317a
Andrew Loeliger <loeliger@wisc.edu>
2023-10-25 10:12:26 -0500

    Add CaloSummary Emulation to the L1Ntuples Emulator tree

--- L1Trigger/L1TNtuples/src/classes.h
+++ L1Trigger/L1TNtuples/src/classes.h
@@ -31,3 +31,5 @@
 #include "L1Trigger/L1TNtuples/interface/L1AnalysisRecoElectronDataFormat.h"
 
 #include "L1Trigger/L1TNtuples/interface/L1AnalysisL1Upgrade.h"
+
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisCaloSummaryDataFormat.h"
